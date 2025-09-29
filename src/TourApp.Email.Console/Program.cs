65fb6e35a208ca12ad3fb5b51129b919c2fdedf5
Philip Knudsen <philipalexknudsen@gmail.com>
2025-09-29 11:06:18 +0200

    add email service

---
+++ src/TourApp.Email.Console/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace TourApp.Admin.Console;
+
+public static class Program
+{
+  public static async Task Main(string[] argv)
+  {
+    var factory = new ConnectionFactory()
+    {
+      HostName = "localhost",
+      UserName = "user",
+      Password = "password"
+    };
+
+    using var connection = await factory.CreateConnectionAsync();
+    using var channel = await connection.CreateChannelAsync();
+
+    await channel.QueueDeclareAsync("tour.email", durable: true, exclusive: false, autoDelete: false);
+    await channel.QueueBindAsync("tour.email", "tour.topic", "tour.booked");
+
+    var consumer = new AsyncEventingBasicConsumer(channel);
+
+    consumer.ReceivedAsync += async (model, ea) =>
+    {
+      var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+
+      System.Console.WriteLine(message);
+
+      await channel.BasicAckAsync(ea.DeliveryTag, false);
+    };
+
+    await channel.BasicConsumeAsync("tour.email", autoAck: false, consumer: consumer);
+
+    System.Console.ReadKey();
+  }
+}
